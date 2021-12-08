a999334eaf2fd606520ef1d36df5d30a81bbc72b
MarlonDValencia <marlondvalencia@gmail.com>
2021-12-08 17:31:41 -0500

    Calculadora terminada

---
+++ index.js
@@ -0,0 +1,19 @@
+const suma = (FirstValue,SecondValue) => FirstValue+SecondValue
+const resta = (FirstValue,SecondValue) => FirstValue-SecondValue
+
+const multiplicar = (FirstValue,SecondValue) => {
+  const range = (FirstValue, SecondValue) => {
+    if(SecondValue===1) return [FirstValue];
+    return [FirstValue, ...range(FirstValue, resta(SecondValue,1))];
+  }
+  return range(FirstValue,SecondValue).reduce(suma,0)
+}
+
+function dividir(FirstValue,SecondValue){
+    const range = (FirstValue, SecondValue) => {
+      if(resta(FirstValue,SecondValue)<SecondValue) return [FirstValue];
+      return [FirstValue, ...range(resta(FirstValue,SecondValue), SecondValue)];
+    }
+    return (range(FirstValue,SecondValue)).length
+}
+console.log(dividir(10,2))
